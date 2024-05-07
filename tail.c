7b4ee9f5ffe54c35cfd396d6165e73faa739fe37
Hexagoritm <139809645+Hexagorithm@users.noreply.github.com>
2024-05-07 14:19:09 +0200

    Add specific option detection

--- tail.c
+++ tail.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h> //strlen
 /* Options: */
 /*
  * -n <uint> 
@@ -11,8 +12,39 @@ int main(int argc, char* argv[])
 	for (int _ = 1; _ < argc; ++_)
 	{
 		argument = *(++argv);
-		printf("Argument found!\n");
-		printf("Argument found: %s\n",argument);
+		if (argument[0] = '-') /* Argument is an option*/
+		{
+			if (strlen(argument) == 1)
+			{
+				printf("Invalid argument: \"-\"\n");
+				return 1;
+
+			}
+			switch (argument[1])
+			{
+				case 'n':
+					if (strlen(argument) != 2)
+					{
+						printf("Invalid argument detected: \"%s\".\n",argument);
+						return 1;
+					}
+					printf("-n option detected.\n");
+					break;
+				case 'h':
+					if (strlen(argument) != 2)
+					{
+						printf("Invalid argument detected: \"%s\".\n",argument);
+						return 1;
+					}
+					printf("-h option detected.\n");
+					break;
+				default:
+					printf("Invalid argument detected: \"%s\".\n",argument);
+					return 1;
+
+			}
+		}
+
 	}
 	return 0;
 }
